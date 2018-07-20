a5b9be640b0a7fe214938cccda2d4f4f7a293b50
Peter D. Gray <peter@conalgo.com>
2018-07-20 09:58:41 -0400

    Revised for final BIP174 format details

--- psbt_dump.py
+++ psbt_dump.py
@@ -18,6 +18,23 @@ from pycoin.tx.TxOut import TxOut
 from pycoin.encoding import b2a_hashed_base58, hash160
 from pycoin.serialize import b2h_rev
 
+# BIP-174 aka PSBT defined values
+PSBT_GLOBAL_UNSIGNED_TX 	= (0)
+
+PSBT_IN_NON_WITNESS_UTXO 	= (0)
+PSBT_IN_WITNESS_UTXO 	    = (1)
+PSBT_IN_PARTIAL_SIG 	    = (2)
+PSBT_IN_SIGHASH_TYPE 	    = (3)
+PSBT_IN_REDEEM_SCRIPT 	    = (4)
+PSBT_IN_WITNESS_SCRIPT 	    = (5)
+PSBT_IN_BIP32_DERIVATION 	= (6)
+PSBT_IN_FINAL_SCRIPTSIG 	= (7)
+PSBT_IN_FINAL_SCRIPTWITNESS = (8)
+
+PSBT_OUT_REDEEM_SCRIPT 	    = (0)
+PSBT_OUT_WITNESS_SCRIPT 	= (1)
+PSBT_OUT_BIP32_DERIVATION 	= (2)
+
 b2a_hex = lambda a: str(_b2a_hex(a), 'ascii')
 
 def deser_compact_size(f, nit=None):
@@ -49,6 +66,7 @@ def dump(psbt, hex_output, testnet):
     
     print("%d bytes in PSBT: %s" % (len(raw), psbt.name))
 
+
     with io.BytesIO(raw) as fd:
         hdr = fd.read(4)
         sep1 = fd.read(1)
@@ -56,12 +74,15 @@ def dump(psbt, hex_output, testnet):
         print("-- HEADER --\n\n%s 0x%02x\n" % (str(hdr, 'ascii'), sep1[0]))
 
         print("-- GLOBALS --")
-        sect = 0
+        num_ins = None
+        num_outs = None
+        section = 'globals'
+        section_idx = 0
 
         while 1:
             first = fd.read(1)
             if first == b'':
-                print("\n-- EOF --")
+                print("-- ACTUAL EOF --")
                 break
 
             try:
@@ -71,8 +92,28 @@ def dump(psbt, hex_output, testnet):
                 break
 
             if ks == 0:
-                sect += 1
-                print("-- SEP -- (below probably for input #%d)" % (sect-1))
+                section_idx += 1
+
+                if section == 'globals':
+                    section_idx = 0
+                    section = 'inputs'
+                    print("-- INPUT #0 --")
+                elif section == 'inputs':
+                    if section_idx == num_ins:
+                        print("-- OUTPUT #0 --")
+                        section = 'outputs'
+                        section_idx = 0
+                    else:
+                        print("-- INPUT #%d --" % section_idx)
+                elif section == 'outputs':
+                    if section_idx == num_outs:
+                        print("-- EXPECT EOF --")
+                        section = 'past eof'
+                        section_idx = 0
+                    else:
+                        print("-- OUTPUT #%d --" % section_idx)
+                else:
+                    print("-- ?? %s ??  --" % section.upper())
                 continue
 
             try:
@@ -88,25 +129,39 @@ def dump(psbt, hex_output, testnet):
                 break
 
             try:
-                if sect == 0:
-                    purpose = ['Transaction', 'Redeem Script', 'Witness Script',
-                                    'HD Path', 'Num PSBT Inputs'][key[0]]
-                else:
-                    purpose = ['UTXO', 'Witness UTXO', 'Parial Sig',
-                                    'Sighash Type', 'Input Index'][key[0]]
-            except IndexError:
-                purpose = 'UNKNOWN'
+                if section == 'globals':
+                    purpose = ['GLOBAL_UNSIGNED_TX'][key[0]]
+                elif section == 'inputs':
+                    purpose = [ 'IN_NON_WITNESS_UTXO',
+                                'IN_WITNESS_UTXO',
+                                'IN_PARTIAL_SIG',
+                                'IN_SIGHASH_TYPE',
+                                'IN_REDEEM_SCRIPT',
+                                'IN_WITNESS_SCRIPT',
+                                'IN_BIP32_DERIVATION',
+                                'IN_FINAL_SCRIPTSIG',
+                                'IN_FINAL_SCRIPTWITNESS'][key[0]]
+                elif section == 'outputs':
+                    purpose = [ 'OUT_REDEEM_SCRIPT',
+                                'OUT_WITNESS_SCRIPT',
+                                'OUT_BIP32_DERIVATION'][key[0]]
 
-            print('\n  key: %02x %s (%s bytes, %r)' % (
-                        key[0], b2a_hex(key[1:]) if len(key) > 1 else '', ks, purpose))
+            except IndexError:
+                purpose = 'Unknown type=0x%0x' % key[0]
+
+            print('\n  key: %02x %s (%s%s)' % (
+                        key[0], b2a_hex(key[1:]) if len(key) > 1 else '',
+                        purpose,
+                        (', %d bytes' % ks if ks != 1 else '')))
             print('value:\n\n%s  (%d bytes)\n' % (b2a_hex(val), vs))
 
             # prefix byte for addresses in current network
             ADP = b'\x6f' if testnet else b'\0'
 
-            if (sect, key[0]) == (0, 0) or key[0] == 0:
+            if (section, key[0]) in [ ('globals', PSBT_GLOBAL_UNSIGNED_TX),
+                                      ('inputs', PSBT_IN_NON_WITNESS_UTXO)]:
                 # Parse and sumarize the bitcoin transaction.
-                # - also works for UTXO on the outputs side
+                # - also works for non-witness UTXO 
                 
                 try:
                     t = Tx.parse(io.BytesIO(val))
@@ -115,17 +170,22 @@ def dump(psbt, hex_output, testnet):
                                     sum(1 for i in t.txs_in if i.witness)))
                     print("            : txid %s" % t.hash())
                     for n,i in enumerate(t.txs_in):
-                        print("   [in #%2d] %s" % (n, i.address(ADP) if i.script else '(not signed)'))
-                        if sect == 0:
+                        print("   [in #%-2d] %s" % (n, i.address(ADP) if i.script else '(not signed)'))
+                        if section == 'globals':
                             print("            from %s : %d" % (b2h_rev(i.previous_hash), i.previous_index))
                     for n,o in enumerate(t.txs_out):
-                        print("  [out #%d] %s" % (n, o.address('XTN' if testnet else 'BTC')))
+                        print("  [out #%-2d] %s" % (n, o.address('XTN' if testnet else 'BTC')))
                     print("\n")
-                except:
-                    raise
-                    print("(unable to parse txn)")
 
-            if sect == 0 and key[0] == 0x3:
+                    if num_ins is None:
+                        num_ins = len(t.txs_in)
+                        num_outs = len(t.txs_out)
+                except:
+                    print("(unable to parse txn)")
+                    raise
+
+            if (section, key[0]) in [('inputs', PSBT_IN_BIP32_DERIVATION),
+                                     ('outputs', PSBT_OUT_BIP32_DERIVATION)]:
                 # HD key paths
                 try:
                     pubkey = key[1:]
@@ -148,8 +208,7 @@ def dump(psbt, hex_output, testnet):
                 except:
                     print("(unable to parse hdpath)")
 
-            
-
 if __name__ == '__main__':
     dump()
 
+# EOF
