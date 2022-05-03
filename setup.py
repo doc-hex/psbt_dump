7ddedfa8335e7744aa1817151614b2020c95323a
Peter D. Gray <peter@conalgo.com>
2022-05-03 11:32:08 -0400

    better packaging

--- setup.py
+++ setup.py
@@ -4,20 +4,37 @@
 #
 #   pip install --editable .
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    name='psbt_dump',
-    version='1.0',
-    py_modules=[],
-    python_requires='>3.5.2',
-    install_requires=[
-        'Click',
-        'pycoin == 0.80'
-    ],
-    entry_points='''
-        [console_scripts]
-        psbt_dump=psbt_dump:dump
-    ''',
-)
+VERSION = '1.5'
+
+with open('README.md', 'rt') as fd:
+    desc = fd.read()
+
+if __name__ == '__main__':
+    setup(
+        name='psbt_dump',
+        author='Coinkite Inc.',
+        author_email='support@coinkite.com',
+        description="Dump PSBT files to text with as few assumptions as possible",
+        version=VERSION,
+        packages=find_packages(),
+        long_description=desc,
+        long_description_content_type="text/markdown",
+        url="https://github.com/doc-hex/psbt_dump",
+        python_requires='>3.6.0',
+        install_requires=[
+            'Click',
+            'pycoin == 0.80'
+        ],
+        entry_points='''
+            [console_scripts]
+            psbt_dump=psbt_dump:dump
+        ''',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ]
+    )
 
