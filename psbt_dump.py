bfe1c8091bea3b44e7a2e793437b945a9ed507d1
scgbckbone <scgbckbone@proton.me>
2023-05-05 11:26:19 +0200

    PSBTv2

--- psbt_dump.py
+++ psbt_dump.py
@@ -8,7 +8,6 @@
 #
 #
 import click, sys, pdb, struct, io, copy
-from io import BytesIO
 from binascii import b2a_hex as _b2a_hex
 from binascii import a2b_hex as _a2b_hex
 from base64 import b64encode, b64decode
@@ -22,34 +21,74 @@ from segwit_addr import encode as bech32_encode
 Tx = network.Tx
 
 # BIP-174 aka PSBT defined values
-PSBT_GLOBAL_UNSIGNED_TX 	  = 0
-PSBT_GLOBAL_XPUB        	  = 1
+#
+# GLOBAL ===
+PSBT_GLOBAL_UNSIGNED_TX 	     = 0x00
+PSBT_GLOBAL_XPUB        	     = 0x01
+PSBT_GLOBAL_VERSION              = 0xfb
+PSBT_GLOBAL_PROPRIETARY          = 0xfc
+# BIP-370
+PSBT_GLOBAL_TX_VERSION           = 0x02
+PSBT_GLOBAL_FALLBACK_LOCKTIME    = 0x03
+PSBT_GLOBAL_INPUT_COUNT          = 0x04
+PSBT_GLOBAL_OUTPUT_COUNT         = 0x05
+PSBT_GLOBAL_TX_MODIFIABLE        = 0x06
 
-PSBT_IN_NON_WITNESS_UTXO 	  = 0
-PSBT_IN_WITNESS_UTXO 	      = 1
-PSBT_IN_PARTIAL_SIG 	      = 2
-PSBT_IN_SIGHASH_TYPE 	      = 3
-PSBT_IN_REDEEM_SCRIPT 	      = 4
-PSBT_IN_WITNESS_SCRIPT 	      = 5
-PSBT_IN_BIP32_DERIVATION 	  = 6
-PSBT_IN_FINAL_SCRIPTSIG 	  = 7
-PSBT_IN_FINAL_SCRIPTWITNESS   = 8
+# INPUTS ===
+PSBT_IN_NON_WITNESS_UTXO 	     = 0x00
+PSBT_IN_WITNESS_UTXO 	         = 0x01
+PSBT_IN_PARTIAL_SIG 	         = 0x02
+PSBT_IN_SIGHASH_TYPE 	         = 0x03
+PSBT_IN_REDEEM_SCRIPT 	         = 0x04
+PSBT_IN_WITNESS_SCRIPT 	         = 0x05
+PSBT_IN_BIP32_DERIVATION 	     = 0x06
+PSBT_IN_FINAL_SCRIPTSIG 	     = 0x07
+PSBT_IN_FINAL_SCRIPTWITNESS      = 0x08
+PSBT_IN_POR_COMMITMENT           = 0x09   # Proof of Reserves
+PSBT_IN_RIPEMD160                = 0x0a
+PSBT_IN_SHA256                   = 0x0b
+PSBT_IN_HASH160                  = 0x0c
+PSBT_IN_HASH256                  = 0x0d
+# BIP-370
+PSBT_IN_PREVIOUS_TXID            = 0x0e
+PSBT_IN_OUTPUT_INDEX             = 0x0f
+PSBT_IN_SEQUENCE                 = 0x10
+PSBT_IN_REQUIRED_TIME_LOCKTIME   = 0x11
+PSBT_IN_REQUIRED_HEIGHT_LOCKTIME = 0x12
 # BIP-371
-PSBT_IN_TAP_KEY_SIG           = 19  # 0x13
-PSBT_IN_TAP_SCRIPT_SIG        = 20  # 0x14
-PSBT_IN_TAP_LEAF_SCRIPT       = 21  # 0x15
-PSBT_IN_TAP_BIP32_DERIVATION  = 22  # 0x16
-PSBT_IN_TAP_INTERNAL_KEY      = 23  # 0x17
-PSBT_IN_TAP_MERKLE_ROOT       = 24  # 0x18
+PSBT_IN_TAP_KEY_SIG              = 0x13
+PSBT_IN_TAP_SCRIPT_SIG           = 0x14
+PSBT_IN_TAP_LEAF_SCRIPT          = 0x15
+PSBT_IN_TAP_BIP32_DERIVATION     = 0x16
+PSBT_IN_TAP_INTERNAL_KEY         = 0x17
+PSBT_IN_TAP_MERKLE_ROOT          = 0x18
 
-PSBT_OUT_REDEEM_SCRIPT 	      = 0
-PSBT_OUT_WITNESS_SCRIPT 	  = 1
-PSBT_OUT_BIP32_DERIVATION 	  = 2
+# OUTPUTS ===
+PSBT_OUT_REDEEM_SCRIPT 	         = 0x00
+PSBT_OUT_WITNESS_SCRIPT 	     = 0x01
+PSBT_OUT_BIP32_DERIVATION 	     = 0x02
+# BIP-370
+PSBT_OUT_AMOUNT                  = 0x03
+PSBT_OUT_SCRIPT                  = 0x04
 # BIP-371
-PSBT_OUT_TAP_INTERNAL_KEY     = 5
-PSBT_OUT_TAP_TREE             = 6
-PSBT_OUT_TAP_BIP32_DERIVATION = 7
+PSBT_OUT_TAP_INTERNAL_KEY        = 0x05
+PSBT_OUT_TAP_TREE                = 0x06
+PSBT_OUT_TAP_BIP32_DERIVATION    = 0x07
 
+v2_FIELDS = (
+    PSBT_GLOBAL_TX_VERSION,
+    PSBT_GLOBAL_FALLBACK_LOCKTIME,
+    PSBT_GLOBAL_INPUT_COUNT,
+    PSBT_GLOBAL_OUTPUT_COUNT,
+    PSBT_GLOBAL_TX_MODIFIABLE,
+    PSBT_IN_PREVIOUS_TXID,
+    PSBT_IN_OUTPUT_INDEX,
+    PSBT_IN_SEQUENCE,
+    PSBT_IN_REQUIRED_TIME_LOCKTIME,
+    PSBT_IN_REQUIRED_HEIGHT_LOCKTIME,
+    PSBT_OUT_AMOUNT,
+    PSBT_OUT_SCRIPT,
+)
 
 global_purpose = {}
 output_purpose = {}
@@ -131,6 +170,9 @@ def render_address(script, testnet=True):
 @click.option('--testnet', '-t', help="Assume testnet3 addresses", is_flag=True, default=False)
 @click.option('--show-addrs', '-a', help="Attempt decode of addresses", is_flag=True, default=False)
 def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
+    has_txn = False
+    version = None
+    v2_fields = False
 
     raw = psbt.read()
     if raw[0:10] == b'70736274ff':
@@ -235,24 +277,54 @@ def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
 
             print('value: ', end='')
 
+            if key[0] in v2_FIELDS:
+                v2_fields = True
+
             if len(val) == 4 and key[0] != PSBT_GLOBAL_XPUB:
                 nn, = struct.unpack("<I", val)
                 if key[0] == PSBT_IN_SIGHASH_TYPE:
                     print("'%s' = 0x%x = %d = %s\n" % (b2a_hex(val), nn, nn, SIGHASH_MAP[nn]))
                 else:
                     print("'%s' = 0x%x = %d\n" % (b2a_hex(val), nn, nn))
+
+                if key[0] == PSBT_GLOBAL_VERSION:
+                    print("\tPSBTv%d\n" % nn)
+                    version = nn
+
                 continue
+
+            if section == "globals" and key[0] == PSBT_GLOBAL_INPUT_COUNT:
+                num_ins = deser_compact_size(io.BytesIO(val))
+                v2_fields = True
+
+            if section == "globals" and key[0] == PSBT_GLOBAL_OUTPUT_COUNT:
+                num_outs = deser_compact_size(io.BytesIO(val))
+                v2_fields = True
 
             print('%s  (%d bytes)\n' % (b2a_hex(val), vs))
 
             # prefix byte for addresses in current network
             ADP = testnetwork.address if testnet else network.address
 
+            if section == "globals" and key[0] == PSBT_GLOBAL_TX_MODIFIABLE:
+                print("\tbitfield: %s\n" % bin(val[0]))
+                if val[0] & 0b1:
+                    print("\tInputs can be modified.")
+                if val[0] & 0b10:
+                    print("\tOutputs can be modified.")
+                if val[0] & 0b100:
+                    print("\tHas SIGHASH_SINGLE flag.")
+                if val[0] & 0xf8:
+                    print("\tUndefined bit flag set.")
+                print()
+
             if (section, key[0]) in [ ('globals', PSBT_GLOBAL_UNSIGNED_TX),
                                       ('inputs', PSBT_IN_NON_WITNESS_UTXO)]:
                 # Parse and sumarize the bitcoin transaction.
                 # - also works for non-witness UTXO 
-                
+                if key[0] == PSBT_GLOBAL_UNSIGNED_TX:
+                    has_txn = True
+
                 try:
                     t = Tx.parse(io.BytesIO(val))
                     print(" Transaction: (%d inputs, %d outputs, %d witness)" % (
@@ -276,6 +348,16 @@ def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
                     print("(unable to parse txn)")
                     raise
 
+            if section == "outputs" and key[0] == PSBT_OUT_AMOUNT:
+                sats = struct.unpack("<q", val)[0]
+                print('\t%d sats\t%d\n' % (sats, sats / 100000000))
+                continue
+
+            if section == "outputs" and key[0] == PSBT_OUT_SCRIPT:
+                addr = render_address(val, testnet)
+                print('\t%s\n' % addr)
+                continue
+
             if (section, key[0]) == ('globals', PSBT_GLOBAL_XPUB):
                 # key is: binary BIP32 serialization (not base58)
                 # value is: master key fingerprint catenated with the derivation path of public key
@@ -292,7 +374,7 @@ def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
                 print("    HD Path: (m=%s)/%s\n" % (xfp2hex(fingerprint), '/'.join(path)))
 
             if (section, key[0]) == ("outputs", PSBT_OUT_TAP_TREE):
-                reader = BytesIO(val)
+                reader = io.BytesIO(val)
                 print("    Output Taproot Tree:")
                 while True:
                     depth = reader.read(1)
@@ -336,7 +418,7 @@ def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
                 # taproot HD key paths
                 try:
                     xonly_pubkey = key[1:]
-                    reader = BytesIO(val)
+                    reader = io.BytesIO(val)
                     leaf_hash_len = deser_compact_size(reader)
                     leaf_hashes = []
                     for _ in range(leaf_hash_len):
@@ -416,6 +498,17 @@ def dump(psbt, hex_output, bin_output, testnet, base64, show_addrs):
                         # XXX decode implied p2sh addresses here too?
                 except:
                     print("(unable to parse POSSIBLE multisig redeem script)")
+    print()
+    if version is not None:
+        if version == 0 and v2_fields:
+            print("INVALID: PSBT v0 requires exclusion of PSBT v2 fields.")
+        elif version == 2 and has_txn:
+            print("INVALID: PSBT v2 requires exclusion of PSBT_GLOBAL_UNSIGNED_TX")
+    else:
+        if has_txn and v2_fields:
+            print("INVALID: PSBT contains PSBT_GLOBAL_UNSIGNED_TX together with PSBT v2 fields.")
+
+
 
 if __name__ == '__main__':
     dump()
